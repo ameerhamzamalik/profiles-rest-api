136c4d01700833f6642f460be777f5ad98d0810c
hamza <ameerhamza630@ymail.com>
2020-09-21 01:51:14 +0500

    Enable Django admin for User Profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -32,7 +32,7 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
